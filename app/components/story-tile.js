afa10b0becb24c547f748deb64000d8662cbe17f
simon <simonctemple@live.com>
2016-03-30 13:22:45 -0700

    add delete functionality

--- app/components/story-tile.js
+++ app/components/story-tile.js
@@ -8,6 +8,11 @@ export default Ember.Component.extend({
     },
     imageHide: function() {
       this.set('isImageShowing', false);
+    },
+    delete(story) {
+      if (confirm('Are you sure you want to delete this story?')) {
+        this.sendAction('destroyStory', story);
+      }
     }
   }
 });
