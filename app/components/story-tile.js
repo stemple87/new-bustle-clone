0f98a5f4bb1801f1ed26107377340db78cf21932
simon <simonctemple@live.com>
2016-03-30 15:25:34 -0700

    add update functionality

--- app/components/story-tile.js
+++ app/components/story-tile.js
@@ -2,12 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isImageShowing: false,
+  updateStoryForm: false,
   actions: {
     imageShow: function() {
       this.set('isImageShowing', true);
     },
     imageHide: function() {
       this.set('isImageShowing', false);
+    },
+    update(story, params) {
+      this.sendAction('update', story, params);
     },
     delete(story) {
       if (confirm('Are you sure you want to delete this story?')) {
---
+++ app/components/update-story.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updateStoryForm: false,
+  actions: {
+    updateStoryForm() {
+      this.set('updateStoryForm', true);
+    },
+    update(story) {
+      var params = {
+        headline: this.get('headline'),
+        author: this.get('author'),
+        body: this.get('body'),
+        image: this.get('image'),
+      };
+      this.set('updateStoryForm', false);
+      this.sendAction('update', story, params);
+    }
+  }
+});
